2945a5f328c01f886f668bbcd15e6b3928692fc4
Francis Deslauriers <frdeso@beta>
2012-12-30 00:42:41 -0500

    Indentation
    Signed-off-by: Francis Deslauriers <frdeso@beta>

--- upload.php
+++ upload.php
@@ -1,17 +1,17 @@
 <?php
-        $target = getcwd();
-	$target = $target."/upload/";
-	$newFileName = substr(md5(microtime()),rand(0,26),5);
-	$newFileName = $newFileName . ".jpg";
-	$target = $target . basename( $newFileName) ; 
-	echo $target.PHP_EOL;
-	
-	if(move_uploaded_file($_FILES['uploaded']['tmp_name'], $target)) 
-	{
-		echo $target;
-		echo "Success".PHP_EOL;
-	} 
-	else {
-		echo "Failure".PHP_EOL;
-	}
+    $target = getcwd();
+    $target = $target."/upload/";
+    $newFileName = substr(md5(microtime()),rand(0,26),5);
+    $newFileName = $newFileName . ".jpg";
+    $target = $target . basename( $newFileName) ;
+    echo $target.PHP_EOL;
+
+    if(move_uploaded_file($_FILES['uploaded']['tmp_name'], $target))
+    {
+            echo $target;
+            echo "Success".PHP_EOL;
+    }
+    else {
+            echo "Failure".PHP_EOL;
+    }
 ?> 
